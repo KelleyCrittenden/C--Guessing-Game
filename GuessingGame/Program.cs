751ce42723d6449e5a00c0409d0eb49e4518c173
Kelley Crittenden <kelleylcrittenden@gmail.com>
2020-08-25 11:43:13 -0500

    Phase Six Complete

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -15,14 +15,24 @@ namespace GuessingGame
 
             while (numberOfAttempts < 4)
             {
-                Console.WriteLine($"You have {4 - numberOfAttempts} guesses left .");
+                Console.WriteLine($"Attempts Remaining: {4 - numberOfAttempts}.");
 
                 int numberGuess = Int32.Parse(Console.ReadLine());
 
                 if (numberGuess == secretNumber)
                 {
                     Console.WriteLine("Congrats, you guessed the secret number!");
-                    numberOfAttempts = 4;
+                    break;
+                }
+                else if (numberGuess > secretNumber)
+                {
+                    Console.WriteLine("Your guess was too high");
+                    numberOfAttempts++;
+                }
+                else if (numberGuess < secretNumber)
+                {
+                    Console.WriteLine("Your guess was too low");
+                    numberOfAttempts++;
                 }
                 else
                 {
