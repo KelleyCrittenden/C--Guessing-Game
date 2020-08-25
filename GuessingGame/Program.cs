c3de07ab6cbb0d8f15ccbe15bfc4e8c95d81b167
Kelley Crittenden <kelleylcrittenden@gmail.com>
2020-08-25 10:59:30 -0500

    Phase Three Complete

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -6,23 +6,28 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Can you guess the secret number?");
-
-            string numberGuess = Console.ReadLine();
-
-            // Console.WriteLine($"Your guess was... {numberGuess}");
 
             string secretNumber = "42";
 
-            if (numberGuess == secretNumber)
-            {
-                Console.WriteLine("Congrats, you guessed the secret number!");
-            }
-            else
-            {
-                Console.WriteLine("Try Again!");
-            }
+            int numberAttempts = 0;
 
+            while (numberAttempts < 4)
+            {
+                Console.WriteLine("Can you guess the secret number?");
+
+                string numberGuess = Console.ReadLine();
+
+                if (numberGuess == secretNumber)
+                {
+                    Console.WriteLine("Congrats, you guessed the secret number!");
+                }
+                else
+                {
+                    numberAttempts++;
+                    Console.WriteLine("Try Again!");
+                }
+
+            }
         }
     }
 }
